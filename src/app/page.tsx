26e7eaeed0247bd1242bc2e11c21f02398ea5f49
Lucas Källberg <lucas.arne@hotmail.com>
2023-12-15 14:45:32 +0100

    created hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import TitledParagraph from "@/components/TiltledParagraph/titledParagraph";
+import HeroHome from "@/components/HeroHome/heroHome";
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <TitledParagraph backgroundColor="#ff66c4"></TitledParagraph>
+      <HeroHome></HeroHome>
+      <TitledParagraph backgroundColor="#ffffff"></TitledParagraph>
     </main>
   );
 }
