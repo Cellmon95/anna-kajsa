7458ba89160d101566ff97158b2a3094863a61d9
Cellmon95 <lucas.arne@hotmail.com>
2023-12-18 12:06:07 +0100

    added quote

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import TitledParagraph from "@/components/TiltledParagraph/titledParagraph";
+import Quote from "@/components/Quote/Quote";
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <TitledParagraph></TitledParagraph>
+      <Quote></Quote>
     </main>
   );
 }
---
+++ src/components/Quote/Quote.tsx
@@ -0,0 +1,19 @@
+"use client";
+import styles from "./quote.module.css"
+
+export default function Quote() {
+
+
+
+    return (<>
+    <div className={styles.quoteWrapper}>
+        <p className={styles.quote}>
+            “Be who you are and say what you feel, because those who mind don't matter, and those who matter don't mind.”
+        </p>
+        <p className={styles.author}>
+        Bernard M. Baruch
+        </p>
+    </div>
+
+    </>);
+}
