419552e7dc7f91aa0c5575a8be20d0318f03d709
Lucas Källberg <lucas.arne@hotmail.com>
2023-12-19 14:26:12 +0100

    worked on fetching

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,24 @@
-"use client"
-
 import Image from "next/image";
 import styles from "./page.module.css";
 import TitledParagraph from "@/components/TiltledParagraph/titledParagraph";
 import Quote from "@/components/Quote/Quote";
-import {client} from "../../sanity/lib/client";
+import { client } from "../../sanity/lib/client";
 import HeroHome from "@/components/HeroHome/heroHome";
 
-export default function Home() {
-  async function fetch() {
-    const data = await client.fetch("*[_type == 'page' && title=='Home Page']");
-    console.log(data);
-  }
-
-  fetch();
-
+export default async function Home() {
+  const pageData = await fetchData();
+  console.log(pageData);
   return (
     <main className={styles.main}>
-      <Quote></Quote>
-      <HeroHome></HeroHome>
-      <TitledParagraph backgroundColor="#ffffff"></TitledParagraph>
+      <h1></h1>
     </main>
   );
+}
+
+export async function fetchData() {
+  const pageData = await client.fetch(
+    "*[_type == 'page' && slug.current=='home-page']"
+  );
+
+  return pageData;
 }
