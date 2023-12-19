921bf3dc185f9a8518a257f264177700dc7e3411
Lucas Källberg <lucas.arne@hotmail.com>
2023-12-19 14:29:09 +0100

    changed ' in Quote

--- src/components/Quote/Quote.tsx
+++ src/components/Quote/Quote.tsx
@@ -1,17 +1,16 @@
 "use client";
-import styles from "./quote.module.css"
+import styles from "./quote.module.css";
 
 export default function Quote() {
-
-    return (<>
-    <div className={styles.quoteWrapper}>
+  return (
+    <>
+      <div className={styles.quoteWrapper}>
         <p className={styles.quote}>
-            “Be who you are and say what you feel, because those who mind don't matter, and those who matter don't mind.”
+          “Be who you are and say what you feel, because those who mind
+          don&#39;t matter, and those who matter don&#39;t mind.”
         </p>
-        <p className={styles.author}>
-        Bernard M. Baruch
-        </p>
-    </div>
-
-    </>);
+        <p className={styles.author}>Bernard M. Baruch</p>
+      </div>
+    </>
+  );
 }
