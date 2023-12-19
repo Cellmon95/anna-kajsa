2dbf629f1e1b8357e42c7fd14a6502757478f5a5
Lucas Källberg <lucas.arne@hotmail.com>
2023-12-19 14:33:43 +0100

    Update Quote.tsx

--- src/components/Quote/Quote.tsx
+++ src/components/Quote/Quote.tsx
@@ -6,8 +6,7 @@ export default function Quote() {
     <>
       <div className={styles.quoteWrapper}>
         <p className={styles.quote}>
-          “Be who you are and say what you feel, because those who mind
-          don&#39;t matter, and those who matter don&#39;t mind.”
+
         </p>
         <p className={styles.author}>Bernard M. Baruch</p>
       </div>
