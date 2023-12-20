9af00a43ab627acc058e8ed5729b94df8834704c
Robin Persson <robinpn98@hotmail.se>
2023-12-20 14:03:57 +0100

    added props to heroComponent

--- src/components/Quote/Quote.tsx
+++ src/components/Quote/Quote.tsx
@@ -3,14 +3,15 @@ import styles from './quote.module.css';
 
 interface QuoteProps {
   quote: string;
+  author: string;
 }
 
 export default function Quote(props: QuoteProps) {
   return (
     <>
       <div className={styles.quoteWrapper}>
-        <p className={styles.quote}></p>
-        <p className={styles.author}>{props.quote}</p>
+        <p className={styles.quote}>{props.quote}</p>
+        <p className={styles.author}>{props.author}</p>
       </div>
     </>
   );
