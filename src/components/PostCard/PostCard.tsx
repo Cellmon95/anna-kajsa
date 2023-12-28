cc63af3a5af87aecbc5f76e26a43b51e65e0261d
Robin Persson <robinpn98@hotmail.se>
2023-12-28 21:48:16 +0100

    added routing on card

--- src/components/PostCard/PostCard.tsx
+++ src/components/PostCard/PostCard.tsx
@@ -1,17 +1,25 @@
 'use client';
 import React from 'react';
 import styles from './PostCard.module.css';
+import { useRouter } from 'next/navigation';
 
 interface PostCardProps {
   title: string;
   description: string;
   image?: string;
-  link?: string;
 }
 
 const PostCard = (props: PostCardProps) => {
+  const router = useRouter();
+
   return (
-    <button className={styles.btn}>
+    <button
+      onClick={() => {
+        // will probably change to something like [page]/[post]
+        router.push('/consult');
+      }}
+      className={styles.btn}
+    >
       <h1 className={styles.title}>{props.title}</h1>
       <div className={styles.infoContainer}>
         <img className={styles.img} src={props.image} alt="post cover image" />
